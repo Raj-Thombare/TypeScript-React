36821b8da23ad9b9c1a1d10e0275f6df1b57f774
Rakaa <therajthombare@gmail.com>
2023-01-04 23:57:59 +0530

    children prop

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 
-const Todos: React.FC<{ items: string[] }> = ({ items }) => {
+interface Props {
+  items: string[];
+  children?: React.ReactNode;
+}
+
+const Todos: React.FC<Props> = ({ items, children }) => {
   return (
     <ul>
       {items.map((item) => (
         <li>{item}</li>
       ))}
+      {children}
     </ul>
   );
 };
